8d77a311555f287eef078a067be458d2d3bdc163
TuomasLehti <tuomas.a.lehti@gmail.com>
2019-01-16 20:16:16 +0200

    Create Naiivi Viron lippu.js

---
+++ Naiivi Viron lippu.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(410, 310);
+}
+
+function draw() {
+  background(200);
+  noStroke();
+	fill(0, 0, 255);
+  rect(5, 5, 400, 100);
+  fill(0);
+  rect(5, 105, 400, 100);
+  fill(255);
+  rect(5, 205, 400, 100);
+}
