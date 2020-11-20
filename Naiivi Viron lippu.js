f5ef3cd466adac1186cbd1b9a043757cb893b581
TuomasLehti <tuomas.a.lehti@gmail.com>
2020-11-20 15:20:50 +0200

    Muutettu numeroarvoja
    Muutettu numeroarvoja vastamaan muuta kirjallista materiaalia ja luento-osuutta.

--- Naiivi Viron lippu.js
+++ Naiivi Viron lippu.js
@@ -1,14 +1,14 @@
 function setup() {
-  createCanvas(410, 310);
+  createCanvas(500, 400);
 }
 
 function draw() {
   background(200);
   noStroke();
   fill(0, 0, 255);
-  rect(5, 5, 400, 100);
+  rect(0, 0, 400, 100);
   fill(0);
-  rect(5, 105, 400, 100);
+  rect(0, 100, 400, 100);
   fill(255);
-  rect(5, 205, 400, 100);
+  rect(0, 200, 400, 100);
 }
